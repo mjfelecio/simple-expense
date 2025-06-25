374a52088312fc1902e885d7ed6de491f6656b45
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-25 22:04:58 +0800

    feat: Replace category icon with 'category' icon from MUI

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,6 +7,7 @@ import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -30,7 +31,7 @@ export default function TabLayout() {
         name="categories"
         options={{
           title: 'Categories',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ color }) => <MaterialIcons size={28} name="category" color={color} />,
         }}
       />
       <Tabs.Screen
