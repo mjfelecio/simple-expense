56311782600f79f5e487d50f31ca251e43d81fe2
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-26 21:17:28 +0800

    feat: Create CategoryCard component
    This represents a single category to be displayed on the categories screen

---
+++ components/ui/CategoryCard.tsx
@@ -0,0 +1,18 @@
+import { MaterialIcons } from "@expo/vector-icons";
+import { Text, View } from "react-native";
+import IconCircle from "./IconCircle";
+
+type Props = {
+  icon: keyof typeof MaterialIcons.glyphMap;
+  iconColor: string;
+  name: string;
+};
+
+export default function CategoryCard({ icon, iconColor, name }: Props) {
+  return (
+    <View className="flex flex-row items-center gap-4 p-4">
+      <IconCircle icon={icon} color={iconColor}/>
+      <Text className="text-white text-2xl font-medium">{name}</Text>
+    </View>
+  );
+}
