e46600a748fda6ea23cedec01363aefedfa31b3c
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-26 10:23:05 +0800

    feat: Set home screen as the starting page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -14,6 +14,7 @@ export default function TabLayout() {
 
   return (
     <Tabs
+      initialRouteName='index'
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
