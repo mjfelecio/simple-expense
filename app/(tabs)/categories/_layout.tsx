9a8b3151c3de6cc81c4e335526f043074ca00417
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-26 11:25:56 +0800

    feat: Manually add header for Categories screen
    - Manually rendered custom header above top tabs
    - Applied safe area padding to avoid status bar overlap

--- app/(tabs)/categories/_layout.tsx
+++ app/(tabs)/categories/_layout.tsx
@@ -5,6 +5,8 @@ import {
 } from "@react-navigation/material-top-tabs";
 import { ParamListBase, TabNavigationState } from "@react-navigation/native";
 import { withLayoutContext } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const { Navigator } = createMaterialTopTabNavigator();
 
@@ -16,11 +18,38 @@ export const MaterialTopTabs = withLayoutContext<
 >(Navigator);
 
 export default function CategoriesLayout() {
-	return (
-	<MaterialTopTabs>
-	  <MaterialTopTabs.Screen name="one" options={{ title: "Tab One" }} />
-	  <MaterialTopTabs.Screen name="two" options={{ title: "Tab Two" }} />
-	</MaterialTopTabs>
+	// This accounts for the elements in a device that may hide the text above (like status bars)
+  const insets = useSafeAreaInsets();
+
+  return (
+    <View style={{ flex: 1, paddingTop: insets.top }}>
+      <Text style={styles.headerText}>Categories</Text>
+
+      <View style={{ flex: 1 }}>
+        <MaterialTopTabs>
+          <MaterialTopTabs.Screen
+            name="one"
+            options={{ title: "Tab One" }}
+          />
+          <MaterialTopTabs.Screen
+            name="two"
+            options={{ title: "Tab Two" }}
+          />
+        </MaterialTopTabs>
+      </View>
+    </View>
   );
 }
 
+const styles = StyleSheet.create({
+  tabBarLabel: {
+    fontSize: 18,
+  },
+  headerText: {
+    color: "white",
+    fontSize: 30,
+    fontWeight: "bold",
+    paddingBottom: 12,
+		textAlign: "center"
+  },
+});
