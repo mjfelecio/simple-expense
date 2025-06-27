dca7cd7221509a032c49ef13147b60d6149251cc
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-27 22:23:25 +0800

    fix: Text being rendered in a view directly
    It wasn't a text at all but because of using a component before it has been declared.
    I assumed that arrow functions assigned to consts would get hoisted. Leading to this error

--- components/ui/OverviewCard.tsx
+++ components/ui/OverviewCard.tsx
@@ -2,21 +2,9 @@ import React from "react";
 import { Text, View } from "react-native";
 
 const values = {
-	"income": "54,334",
-	"expense": "50,324",
-	"balance": "4,010"
-}
-
-const OverviewCard = () => {
-  return (
-    <View className="border-4 border-white rounded-xl py-4 m-4 h-32 flex-row">
-      <CardItem name="Income" value={values["income"]} />
-      <View className="bg-gray-800 w-1.5 rounded-2xl"></View> {/* Separator */}
-      <CardItem name="Expenses" value={values["expense"]} />
-      <View className="bg-gray-700 w-1.5 rounded-2xl"></View> {/* Separator */}
-      <CardItem name="Balance" value={values["balance"]} />
-    </View>
-  );
+  income: "54,334",
+  expense: "50,324",
+  balance: "4,010",
 };
 
 type ItemCardProps = {
@@ -26,12 +14,22 @@ type ItemCardProps = {
 
 const CardItem = ({ name, value }: ItemCardProps) => {
   return (
-    <>
-      <View className="flex-1 justify-start items-center gap-1 pt-1.5">
-        <Text className="text-white text-xl font-semibold">{name}</Text>
-        <Text className="text-white text-2xl font-bold">{value}</Text>
-      </View>
-    </>
+    <View className="flex-1 justify-start items-center gap-1 pt-1.5">
+      <Text className="text-white text-xl font-semibold">{name}</Text>
+      <Text className="text-white text-2xl font-bold">{value}</Text>
+    </View>
+  );
+};
+
+const OverviewCard = () => {
+  return (
+    <View className="border-4 border-white rounded-xl py-4 m-4 h-32 flex-row">
+      <CardItem name="Income" value={values["income"]} />
+      <View className="bg-gray-800 w-1.5 rounded-2xl"></View>
+      <CardItem name="Expenses" value={values["expense"]} />
+      <View className="bg-gray-700 w-1.5 rounded-2xl"></View>
+      <CardItem name="Balance" value={values["balance"]} />
+    </View>
   );
 };
 
