d81bcfa104a36a3403dc2cf90f8a21100a71ad43
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-27 23:02:27 +0800

    feat: Add header displaying month and year in home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,14 @@
 import OverviewCard from "@/components/ui/OverviewCard";
 import RecordGroup from "@/components/ui/RecordGroup";
-import { ScrollView } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView className="flex-1">
+      <View className="border-b-2 border-white py-2">
+        <Text className="text-4xl font-bold text-white mx-4">June 2025</Text>
+      </View>
       <OverviewCard />
       <ScrollView>
         <RecordGroup />
