84be5ed87eeeba516c41e1821393cb71f5d473ef
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-28 16:18:37 +0800

    feat: Add name form input in Category Form

--- app/categories/[id].tsx
+++ app/categories/[id].tsx
@@ -1,18 +1,32 @@
-import { useLocalSearchParams } from 'expo-router';
-import React from 'react';
-import { Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { useLocalSearchParams } from "expo-router";
+import React from "react";
+import { Text, TextInput, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const CategoryDetails = () => {
-	const { id } = useLocalSearchParams();
+  const { id } = useLocalSearchParams();
 
-	const isEdit = id !== "new"
+  const isEdit = id !== "new";
 
   return (
-	<SafeAreaView>
-	  <Text className="text-white">{isEdit ? "Edit" : "Add"} Category: {id}</Text>
-	</SafeAreaView>
-  )
-}
+    <SafeAreaView className="p-4">
+      {/* Header */}
+      <Text className="text-white text-3xl font-semibold">
+        {isEdit ? "Edit" : "Add"} Category
+      </Text>
+      {/* Form */}
+      <View className="pt-6 gap-2">
+        {/* Name */}
+        <View>
+          <Text className="text-white text-2xl font-semibold">Name</Text>
+          <TextInput
+            placeholder="Enter category name"
+            className="placeholder:text-gray-300 p-2 text-xl text-white border-2 border-white rounded-md"
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default CategoryDetails
+export default CategoryDetails;
