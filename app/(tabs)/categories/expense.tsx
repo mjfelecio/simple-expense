8b48b372225cd0fe9807eb09a1ddbe0d65a686b9
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-28 09:39:33 +0800

    feat: Add floating add category button in expense tab

--- app/(tabs)/categories/expense.tsx
+++ app/(tabs)/categories/expense.tsx
@@ -1,10 +1,23 @@
 import CategoryCard from "@/components/ui/CategoryCard";
-import { ScrollView } from "react-native";
+import IconCircle from "@/components/ui/IconCircle";
+import { ScrollView, TouchableHighlight, View } from "react-native";
 
 export default function TabOneScreen() {
   return (
-	<ScrollView>
-	  <CategoryCard name="Transportation" icon={"local-gas-station"} iconColor="red" />
-	</ScrollView>
+    <View className="flex-1">
+      <ScrollView>
+        <CategoryCard
+          name="Transportation"
+          icon={"local-gas-station"}
+          iconColor="red"
+        />
+      </ScrollView>
+      <TouchableHighlight
+        onPress={() => alert("Add category")}
+        className="absolute bottom-10 right-10"
+      >
+        <IconCircle icon={"add"} color={"gray"} />
+      </TouchableHighlight>
+    </View>
   );
 }
