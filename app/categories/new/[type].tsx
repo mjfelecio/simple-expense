377740e5c8ea9220c9b02ea4a409a8a136d4849e
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-29 08:27:36 +0800

    feat: Add a special new route for each category types
    This just simplifies things at the cost of code duplication.
    But now we can infer if we came through the expense or through the income tab

---
+++ app/categories/new/[type].tsx
@@ -0,0 +1,148 @@
+import CategoryRadioButton from "@/components/ui/CategoryRadioButton";
+import ColorPickerModal from "@/components/ui/ColorPickerModal";
+import IconCircle from "@/components/ui/IconCircle";
+import IconPickerModal from "@/components/ui/IconPickerModal";
+import { DEFAULT_ICON, DEFAULT_ICON_COLOR } from "@/constants/Defaults";
+import { useAppDB } from "@/database/db";
+import { CategoryTypes, IconName } from "@/shared.types";
+import { router, useLocalSearchParams } from "expo-router";
+import React, { useState } from "react";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const AddCategoryForm = () => {
+  const { addCategory } = useAppDB();
+  const { type } = useLocalSearchParams();
+
+  // Aborts navigation if the type is invalid
+  if (type !== "expense" && type !== "income") {
+	alert("Invalid category type");
+	router.back();
+  }
+
+  // Parses the type param into the valid category types
+  const initialCategory = type === "expense" ? "expense" : "income"
+
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [isIconModalVisible, setIsIconModalVisible] = useState(false);
+
+  // Form States
+  const [name, setName] = useState<string | undefined>();
+  const [category, setCategory] = useState<CategoryTypes>(initialCategory);
+  const [iconColor, setIconColor] = useState<string>(DEFAULT_ICON_COLOR);
+  const [selectedIcon, setSelectedIcon] = useState<IconName>(DEFAULT_ICON);
+
+  const handleSubmit = async () => {
+    if (!name || name.trim() === "") {
+      alert("Name must be present");
+      return;
+    }
+
+    try {
+      const categoryData = {
+        name,
+        type: category,
+        color: iconColor,
+        icon: selectedIcon,
+      };
+
+      await addCategory(categoryData);
+      router.back();
+    } catch (error) {
+      console.error(
+        "Failed to add category:",
+        error instanceof Error ? error.message : error
+      );
+    }
+  };
+
+  return (
+    <SafeAreaView className="flex-1 p-6">
+      {/*=== Header ===*/}
+      <Text className="text-white text-3xl font-semibold">
+        Add Category
+      </Text>
+
+      {/*=== Form === */}
+      <View className="pt-6 gap-2">
+        {/* Name */}
+        <View>
+          <Text className="text-white text-2xl font-semibold">Name</Text>
+          <TextInput
+            value={name}
+            onChangeText={setName}
+            placeholder="Enter category name"
+            className="placeholder:text-gray-300 p-2 text-xl text-white border-2 border-white rounded-md"
+          />
+        </View>
+
+        {/* Category Type */}
+        <View>
+          <Text className="text-white text-2xl font-semibold pt-2">Type</Text>
+          <CategoryRadioButton initialValue={category} onSelect={setCategory} />
+        </View>
+
+        {/* Color */}
+        <View>
+          <Text className="text-white text-2xl font-semibold py-2">Color</Text>
+          <View className="flex-row gap-2">
+            {/* Selected Color */}
+            <View
+              style={{
+                backgroundColor: iconColor,
+                borderRadius: 99,
+                width: 50,
+                height: 50,
+              }}
+            ></View>
+            {/* Select Color Button */}
+            <TouchableOpacity onPress={() => setIsModalVisible(true)}>
+              <IconCircle icon={"colorize"} color={"gray"} />
+            </TouchableOpacity>
+          </View>
+          <ColorPickerModal
+            visible={isModalVisible}
+            initialColor={iconColor}
+            onClose={(icon) => {
+              setIconColor(icon);
+              setIsModalVisible(false);
+            }}
+          />
+        </View>
+
+        {/* Icon */}
+        <View>
+          <Text className="text-white text-2xl font-semibold py-2">Icon</Text>
+          <View className="flex-row gap-2">
+            {/* Selected Icon */}
+            <IconCircle icon={selectedIcon} color="transparent" />
+            {/* Select Icon Button */}
+            <TouchableOpacity onPress={() => setIsIconModalVisible(true)}>
+              <IconCircle icon={"palette"} color={"gray"} />
+            </TouchableOpacity>
+          </View>
+          <IconPickerModal
+            visible={isIconModalVisible}
+            onClose={(icon) => {
+              setSelectedIcon(icon);
+              setIsIconModalVisible(false);
+            }}
+            initialIcon={selectedIcon}
+          />
+        </View>
+      </View>
+      <View className="flex flex-row gap-4 self-end">
+        {/* Cancel Button */}
+        <TouchableOpacity onPress={router.back} className=" mt-4">
+          <IconCircle icon={"close"} color={"gray"} type="square" />
+        </TouchableOpacity>
+        {/* Submit Button */}
+        <TouchableOpacity onPress={handleSubmit} className="mt-4">
+          <IconCircle icon={"save"} color={"gray"} type="square" />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default AddCategoryForm;
