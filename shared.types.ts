42afcd90de80c9f1388eb189193ae0767f2f1945
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-07-04 21:55:57 +0800

    feat: Create type RealRecord to replace depreciated Record type
    I will replace the name in a later date

--- shared.types.ts
+++ shared.types.ts
@@ -11,6 +11,15 @@ export type Record = {
   type: CategoryType;
 };
 
+// This is the real record type, the one above is a depreciated version
+export type RealRecord = {
+  id: number;
+  name: string;
+  amount: number;
+  date: string;
+  category_id: number;
+}
+
 export type Category = {
   id: number;
   name: string;
