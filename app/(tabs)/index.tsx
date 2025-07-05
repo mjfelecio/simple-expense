6e6aa02435f8fce78b9591d26b91cc1046181a61
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-07-05 10:57:36 +0800

    feat: Display all records from the db in home page
    This is temporary and will be replaced by the RecordGroup component to display records based on their date.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,37 @@
 import IconCircle from "@/components/ui/IconCircle";
 import OverviewCard from "@/components/ui/OverviewCard";
-import RecordGroup from "@/components/ui/RecordGroup";
+import RecordCard from "@/components/ui/RecordCard";
+import { useAppDB } from "@/database/db";
+import { RealRecord } from "@/shared.types";
 import { Link } from "expo-router";
+import { useEffect, useState } from "react";
 import { ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
+  const { getAllRecords } = useAppDB();
+
+  const [records, setRecords] = useState<RealRecord[]>();
+
+  const fetchRecords = async () => {
+    try {
+      const result = await getAllRecords();
+
+      if (!result) {
+        throw new Error("Failed to fetch records");
+      }
+
+      setRecords(result);
+    } catch (error) {
+      console.error(error);
+      alert("Failed to fetch records");
+    }
+  };
+
+  useEffect(() => {
+    fetchRecords();
+  }, []);
+
   return (
     <SafeAreaView className="flex-1">
       <View className="border-b-2 border-white py-2">
@@ -13,8 +39,14 @@ export default function HomeScreen() {
       </View>
       <OverviewCard />
       <ScrollView>
-        <RecordGroup />
-        <RecordGroup />
+        {records?.map((record) => (
+          <RecordCard
+            key={record.id}
+            name={record.name}
+            amount={record.amount}
+            category_id={record.category_id}
+          />
+        ))}
       </ScrollView>
       <Link href={"/records/new"} asChild>
         <TouchableOpacity className="absolute bottom-10 right-10">
