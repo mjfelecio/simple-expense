5c527840d75520e4a137be50251eb98d52ee6bbe
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-07-07 18:00:52 +0800

    feat: Setup custom css variables in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
   content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background': '#000c1a',
+        'background-dark': '#000410',
+        'background-light': "#00192f",
+        'foreground': '#d4f8ff',
+        'foreground-muted': '#6cbce7',
+        'highlight': '#006da1',
+        'border': '#004f78',
+        'border-muted': '#003352',
+        'primary': '#00c0ff',
+        'secondary': '#fe9140',
+        'danger': '#f17260',
+        'warning': '#b69f00',
+        'success': '#00bc7b',
+        'info': '#619dff',
+      },
+    },
   },
   plugins: [],
 }
